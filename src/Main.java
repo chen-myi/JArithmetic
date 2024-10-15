27db40788022a6d04a2eff9ca5985f58079544f7
chen-myi <3137954749@qq.com>
2024-10-15 11:39:16 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("hello java");
         System.out.println("test two");
+        System.out.println("测试下拉代码");
     }
 }
