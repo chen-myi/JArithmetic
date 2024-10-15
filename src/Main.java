b4055257a520ab3b89467fde374f45377cf54e49
hp <3137954749@qq.com>
2024-10-15 11:32:06 +0800

    第二次测试提交

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("hello java");
+        System.out.println("test two");
     }
 }
