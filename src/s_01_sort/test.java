88510e7b765a7a00c8ba3ea75e2891102426850a
hp <3137954749@qq.com>
2024-12-24 23:38:31 +0800

    10.19-24

---
+++ src/s_01_sort/test.java
@@ -0,0 +1,11 @@
+package s_01_sort;
+
+import java.util.Random;
+
+public class test {
+    public static void main(String[] args) {
+        int randomInt = (int) (Math.random() * 7);
+        System.out.println("随机整数: " + randomInt);
+
+    }
+}
